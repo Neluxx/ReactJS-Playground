7c2f9382044673d5b55f1f60c658c42459e6f69b
Fabian Arndt <Fabian.Arndt96@gmail.com>
2021-04-16 11:43:18 +0200

    changed BrowserRouter to HashRouter to fix Routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 import Navigation from './components/Navigation'
 import Home from './components/Home'
 import Timer from './components/Timer'
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,51 +1,33 @@
-import React, { useState } from 'react'
-import { Navbar, Nav } from 'react-bootstrap'
+import React, { useState } from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 const Navigation = () => {
-  const [activeLink, setActiveLink] = useState('home')
+  // const [activeLink, setActiveLink] = useState("home");
 
-  console.log(activeLink)
+  // console.log(activeLink);
 
   return (
-    <Navbar collapseOnSelect expand='lg' sticky='top' bg='dark' variant='dark'>
-      <Navbar.Brand href='/'>
-        Productivity-Manager
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-      <Navbar.Collapse id='responsive-navbar-nav'>
-        <Nav className='mr-auto'>
-          <Nav.Link
-            className={activeLink === 'home' ? 'active' : ''}
-            href='/'
-            onClick={() => setActiveLink('home')}
-          >
-            Home
-          </Nav.Link>
-          <Nav.Link
-            className={activeLink === 'timer' ? 'active' : ''}
-            href='/timer'
-            onClick={() => setActiveLink('timer')}
-          >
-            Timer
-          </Nav.Link>
-          <Nav.Link
-            className={activeLink === 'pomodoro' ? 'active' : ''}
-            href='/pomodoro'
-            onClick={() => setActiveLink('pomodoro')}
-          >
-            Pomodoro
-          </Nav.Link>
-          <Nav.Link
-            className={activeLink === 'counter' ? 'active' : ''}
-            href='/counter'
-            onClick={() => setActiveLink('counter')}
-          >
-            Counter
-          </Nav.Link>
+    <Navbar collapseOnSelect expand="lg" sticky="top" bg="dark" variant="dark">
+      <LinkContainer to="/">
+        <Navbar.Brand>Focused</Navbar.Brand>
+      </LinkContainer>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <LinkContainer to="/timer">
+            <Nav.Link>Timer</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/pomodoro">
+            <Nav.Link>Pomodoro</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/counter">
+            <Nav.Link>Counter</Nav.Link>
+          </LinkContainer>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
