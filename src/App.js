bc59a9b6acee169c47260d84ed66c06378119375
Fabian Arndt <Fabian.Arndt96@gmail.com>
2021-04-20 15:17:22 +0200

    implement authentication system

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
 import React from "react";
-import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Navigation from "./components/Navigation";
-import Home from "./components/Home";
 import Timer from "./components/Timer";
 import Pomodoro from "./components/Pomodoro";
 import Counter from "./components/Counter";
 import News from "./components/News";
+import Dashboard from "./components/Dashboard";
 import Error from "./components/Error";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import ForgotPassword from "./components/ForgotPassword";
+import UpdateProfile from "./components/UpdateProfile";
+import { Container } from "react-bootstrap";
+import { AuthProvider } from "./contexts/AuthContext";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
-    <React.Fragment>
-      <Router>
+    <Router>
+      <AuthProvider>
         <Navigation />
         <Switch>
-          <Route exact path="/">
-            <Home />
+          <PrivateRoute exact path="/" component={Dashboard} />
+          <PrivateRoute path="/update-profile">
+            <UpdateProfile />
+          </PrivateRoute>
+          <Route path="/signup">
+            <Container
+              className="d-flex align-items-center justify-content-center"
+              style={{ minHeight: "90vh" }}
+            >
+              <div className="w-100" style={{ maxWidth: "400px" }}>
+                <Signup />
+              </div>
+            </Container>
+          </Route>
+          <Route path="/login">
+            <Container
+              className="d-flex align-items-center justify-content-center"
+              style={{ minHeight: "90vh" }}
+            >
+              <div className="w-100" style={{ maxWidth: "400px" }}>
+                <Login />
+              </div>
+            </Container>
+          </Route>
+          <Route path="/forgot-password">
+            <Container
+              className="d-flex align-items-center justify-content-center"
+              style={{ minHeight: "90vh" }}
+            >
+              <div className="w-100" style={{ maxWidth: "400px" }}>
+                <ForgotPassword />
+              </div>
+            </Container>
           </Route>
           <Route path="/timer">
             <Timer />
@@ -35,8 +72,8 @@ function App() {
             <Error />
           </Route>
         </Switch>
-      </Router>
-    </React.Fragment>
+      </AuthProvider>
+    </Router>
   );
 }
 
